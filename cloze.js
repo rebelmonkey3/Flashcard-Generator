a30d3177ff0379d61df2a35e99ce9426038ee0eb
Blake Weisgarber <blakeweisgarber@Blakes-MBP.attlocal.net>
2017-04-07 23:49:03 -0700

    Homework push

---
+++ cloze.js
@@ -0,0 +1,23 @@
+
+
+//Clozecard constructor
+
+function ClozeCard(front, back) {
+	this.front = front;
+	this.back = back;
+
+	// Look at argument 3 (the statement) to see if argument 4 is contained in it.
+	if (indexof.Clozecard >= this[4]){
+		//If arugment 4 is part of argument 3, replace it with "..." .
+		remove(this[4] from index) replace with('...')
+	}
+}
+
+							
+var ClozeCard = new ClozeCard('front', 'back')
+
+console.log(Flashcard)
+
+module.exports = ClozeCard;
+
+
---
+++ flashcard.js
@@ -0,0 +1,22 @@
+
+
+//Flashcard constructor
+
+var Flashcard = [];
+
+function FlashCard(front, back) {
+	this.front = front;
+	this.back = back;
+
+	// this.complete = function(){
+	// 	return '${this.front} ${this.back}'
+	// }
+}
+
+							
+var Flashcard = new Flashcard('front', 'back')
+
+console.log(Flashcard)
+
+module.exports = Flashcard
+
